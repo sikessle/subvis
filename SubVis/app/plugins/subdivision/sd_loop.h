2f52efd431b53de7823fa7794317e007d8082a91
Felix Born <feborn@htwg-konstanz.de>
2015-09-01 16:55:02 +0200

    add loop header

--- SubVis/app/plugins/subdivision/sd_loop.h
+++ SubVis/app/plugins/subdivision/sd_loop.h
@@ -1,3 +1,15 @@
+/**
+ *
+ * @brief Implementation of the Loop subdivision algorithm.
+ *
+ * 1. create edge point for each edge
+ * 2. update all vertices of the old mesh
+ * 2. create new faces (4 new triangle faces replace the old triangle face)
+ * (https://graphics.stanford.edu/~mdfisher/subdivision.html)
+ *
+ */
+
+
 #ifndef SUBVIS_PLUGINS_SUBDIVISION_SD_LOOP_H
 #define SUBVIS_PLUGINS_SUBDIVISION_SD_LOOP_H
 
