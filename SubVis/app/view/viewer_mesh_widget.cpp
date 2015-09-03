35554ca22c7a4b25b17b3c41ada144d3a2ab27be
Simon Kessler <sikessle@htwg-konstanz.de>
2015-09-03 19:34:26 +0200

    very very very slow and basic drawing to test algorithms

--- SubVis/app/view/viewer_mesh_widget.cpp
+++ SubVis/app/view/viewer_mesh_widget.cpp
@@ -1,4 +1,6 @@
-#include <GL/glu.h>
+#include <GL/glut.h>
+#include <QDebug>
+
 #include "view/viewer_mesh_widget.h"
 
 namespace subvis {
@@ -10,30 +12,36 @@ void ViewerMeshWidget::init() {
   // black background
   glClearColor(0, 0, 0, 0);
   glClear(GL_COLOR_BUFFER_BIT);
+  // blue color
+  glColor3f(0.0, 0.5, 1.0);
+  // no lighting
+  glDisable(GL_LIGHTING);
+  // wireframe
+  glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+  // TODO compute the correct scene radius and configure camera.
+  setSceneRadius(10.0);
+  camera()->setZNearCoefficient(0.0001);
+  camera()->setZClippingCoefficient(10.0);
 }
 
+// TODO: We should rather pass a VBO to the graphics card to speed things up.
+// this is just a temporary prototype.
+// This implementation is EXTREMLY SLOW!
 void ViewerMeshWidget::draw() {
-  // TODO: do our own mesh based rendering here.
-  // get mesh: draw_controller->mesh_data().mesh();
+  using Surface_mesh = surface_mesh::Surface_mesh;
+  using Point = surface_mesh::Point;
 
+  const Surface_mesh& mesh =
+    draw_controller_->get_mesh_data().get_mesh();
 
-
-
-  glBegin(GL_POLYGON);
-
-  glDisable(GL_LIGHTING);
-  glColor3f(1.0, 1.0, 1.0);
-  glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-
-  glEnableClientState(GL_VERTEX_ARRAY);
-  //GL::glVertexPointer(mesh_.points());
-
-  //glDrawElements(GL_TRIANGLES, indices_.size(), GL_UNSIGNED_INT, &indices_[0]);
-
-  glDisableClientState(GL_VERTEX_ARRAY);
-  glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-
-  glEnd();
+  for (const auto& face : mesh.faces()) {
+    glBegin(GL_POLYGON);
+    for (const auto& vertex : mesh.vertices(face)) {
+      const Point& p = mesh.get_vertex_property<Point>("v:point")[vertex];
+      glVertex3d(p[0], p[1], p[2]);
+    }
+    glEnd();
+  }
 }
 
 } // namespace subvis
