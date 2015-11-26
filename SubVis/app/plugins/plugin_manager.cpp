a86bf799bcd2a90a427b2e0dc74d7545adeaba45
Simon Kessler <sikessle@htwg-konstanz.de>
2015-11-26 11:39:26 +0100

    Debug messages in plugin manager

--- SubVis/app/plugins/plugin_manager.cpp
+++ SubVis/app/plugins/plugin_manager.cpp
@@ -1,3 +1,5 @@
+#include <QtDebug>
+
 #include "plugins/plugin_manager.h"
 
 namespace subvis {
@@ -16,6 +18,7 @@ void PluginManager::register_plugin(std::unique_ptr<SubVisPlugin> plugin) {
   const QString name = plugin->name();
   plugins_[id] = {name, std::move(plugin)};
   plugins_[id].plugin->set_model(mesh_data_);
+  qDebug() << "Registered plugin:" << name;
 }
 
 } // namespace subvis
