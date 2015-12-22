40eedfebd0283e68091824ffd64766b1a7dd7336
Simon Kessler <sikessle@htwg-konstanz.de>
2015-12-22 15:18:56 +0100

    Cleanup

--- SubVis/app/plugins/subdivision/gl_renderer.cpp
+++ SubVis/app/plugins/subdivision/gl_renderer.cpp
@@ -9,14 +9,10 @@ GLRenderer::~GLRenderer() {
 }
 
 void GLRenderer::mesh_updated(const Surface_mesh& mesh) {
-  // placeholder for custom data structure
   mesh_.reset(new Surface_mesh{mesh});
-  // TODO extract vertices in a custom data structure (member variable) to speed up drawing!
-  qDebug() << "Extracted vertices to custo data structure.";
 }
 
 void GLRenderer::render_mesh_opengl() {
-  // TODO use custom data structure here
   if (mesh_) {
     render();
   }
